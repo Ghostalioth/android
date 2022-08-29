5ba75d9aa3e9d3375acca65ccec2dc7289b019dc
Pranav Vashi <neobuddy89@gmail.com>
2022-08-30 02:26:39 +0530

    SystemUI: Allow to change QS tile label text size [1/2]
    * Useful when using more than default columns.
    
    spkal01: Adapt to our impl
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -34,6 +34,7 @@ import android.provider.Settings
 import android.service.quicksettings.Tile
 import android.text.TextUtils
 import android.util.Log
+import android.util.TypedValue
 import android.view.Gravity
 import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
@@ -50,7 +51,6 @@ import android.widget.TextView
 import androidx.annotation.VisibleForTesting
 import android.annotation.SuppressLint;
 import com.android.settingslib.Utils
-import com.android.systemui.FontSizeUtils
 import com.android.systemui.R
 import com.android.systemui.animation.Interpolators
 import com.android.systemui.animation.LaunchableView
@@ -217,6 +217,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
     private var vertical = false
     private val forceHideCheveron = true
     private var labelHide = false
+    private var labelSize = 14f
 
     init {
         setId(generateViewId())
@@ -230,6 +231,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
         }
 
         labelHide = QSLayoutUtils.getQSTileLabelHide(context)
+        labelSize = QSLayoutUtils.getQSTileLabelSize(context)
 
         if (labelHide)
             gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
@@ -299,8 +301,8 @@ open class QSTileViewImpl @JvmOverloads constructor(
     }
 
     fun updateResources() {
-        FontSizeUtils.updateFontSize(label, R.dimen.qs_tile_text_size)
-        FontSizeUtils.updateFontSize(secondaryLabel, R.dimen.qs_tile_text_size)
+        label.setTextSize(TypedValue.COMPLEX_UNIT_SP, labelSize)
+        secondaryLabel.setTextSize(TypedValue.COMPLEX_UNIT_SP, labelSize)
 
 	if (isA11Style) {
 	    updateA11StyleResources()
