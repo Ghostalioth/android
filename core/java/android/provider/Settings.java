e424c73aede13527f91b829088f29ccbaf957919
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-12 19:10:39 +0800

    settings: Add URI for dashboard styles

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6405,6 +6405,11 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * @hide
+         */
+        public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
