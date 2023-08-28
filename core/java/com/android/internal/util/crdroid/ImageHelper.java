35899d6675f89cf29c9d0fbd13ef95f09f21f1a0
Ghosuto <clash.raja10@gmail.com>
2023-08-28 04:16:39 +0200

    core: Added resizeMaxDeviceSize
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/com/android/internal/util/crdroid/ImageHelper.java
+++ core/java/com/android/internal/util/crdroid/ImageHelper.java
@@ -50,6 +50,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.VectorDrawable;
 import android.net.Uri;
+import android.util.DisplayMetrics;
 import android.os.ParcelFileDescriptor;
 import android.renderscript.Element;
 import android.renderscript.Allocation;
@@ -59,6 +60,10 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.util.Xml;
+import android.view.Display;
+import android.view.SurfaceControl;
+import android.view.View;
+import android.view.WindowManager;
 
 public class ImageHelper {
     private static final int VECTOR_WIDTH = 512;
@@ -172,6 +177,42 @@ public class ImageHelper {
                     middleY - bitmap.getHeight() / 2, paint);
             return new BitmapDrawable(context.getResources(), scaledBitmap);
         }
+    }
+
+        public static Bitmap resizeMaxDeviceSize(Context context, Drawable image) {
+        Bitmap i2b = ((BitmapDrawable) image).getBitmap();
+        return resizeMaxDeviceSize(context, i2b);
+    }
+
+    public static Bitmap resizeMaxDeviceSize(Context context, Bitmap image) {
+        Bitmap imageToBitmap;
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = context.getSystemService(WindowManager.class);
+        wm.getDefaultDisplay().getRealMetrics(metrics);
+        int maxHeight = metrics.heightPixels;
+        int maxWidth = metrics.widthPixels;
+        try {
+            imageToBitmap = RGB565toARGB888(image);
+            if (maxHeight > 0 && maxWidth > 0) {
+                int width = imageToBitmap.getWidth();
+                int height = imageToBitmap.getHeight();
+                float ratioBitmap = (float) width / (float) height;
+                float ratioMax = (float) maxWidth / (float) maxHeight;
+
+                int finalWidth = maxWidth;
+                int finalHeight = maxHeight;
+                if (ratioMax > ratioBitmap) {
+                    finalWidth = (int) ((float)maxHeight * ratioBitmap);
+                } else {
+                    finalHeight = (int) ((float)maxWidth / ratioBitmap);
+                }
+                imageToBitmap = Bitmap.createScaledBitmap(imageToBitmap, finalWidth, finalHeight, true);
+                return imageToBitmap;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
     public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, float roundPx) {
