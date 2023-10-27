Spark-Rom/frameworks_base

SystemUI: Introduce MusicCard in VolumePanel
spkal01: Revised code for A13 added some null checks improved dependencies

Co-authored-by: spkal01 <kalligeross@gmail.com>

packages/SystemUI/src/com/android/systemui/spark/SparkMusic.java
@@ -1,0 +1,205 @@
+/*
+* Copyright (C) 2015 The Android Open Source Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.spark;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.Outline;
+import android.graphics.PorterDuff.Mode;
+import android.media.MediaMetadata;
+import android.media.session.MediaController;
+import android.media.session.PlaybackState;
+import android.os.UserHandle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.NotificationMediaManager;
+import lineageos.util.palette.Palette;
+import com.android.internal.graphics.ColorUtils;
+
+public class SparkMusic extends RelativeLayout implements NotificationMediaManager.MediaListener, Palette.PaletteAsyncListener {
+   private static final boolean DEBUG = true;
+   private static final String TAG = "SparkMusic";
+
+   private Context mContext;
+   private NotificationMediaManager mMediaManager;
+   private MediaController mMediaController;
+   private final Handler mHandler = new Handler();
+
+   private CharSequence mMediaTitle;
+   private CharSequence mMediaArtist;
+   private Drawable mMediaArtwork;
+   private boolean mMediaIsVisible;
+
+   private TextView mTitle;
+   private TextView mArtist;
+   private ImageView mArtwork;
+   private int shadow;
+   private int colorArtwork;
+   private int colorTextIcons;
+
+   private ImageButton mPrevious;
+   private ImageButton mPlayPause;
+   private ImageButton mNext;
+
+   private SparkMusic mBackground;
+
+   public SparkMusic(Context context) {
+       this(context, null);
+   }
+
+   public SparkMusic(Context context, AttributeSet attrs) {
+       this(context, attrs, 0);
+   }
+
+   public SparkMusic(Context context, AttributeSet attrs, int defStyleAttr) {
+       this(context, attrs, defStyleAttr, 0);
+   }
+
+   public SparkMusic(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+       super(context, attrs, defStyleAttr, defStyleRes);
+       if (DEBUG) Log.d(TAG, "New Instance");
+   }
+
+   public void initDependencies(NotificationMediaManager mediaManager, Context context) {
+      mContext = context;
+      mMediaManager = mediaManager;
+      mMediaManager.addCallback(this);
+      updateObjects();
+   }
+
+   /**
+    * Called whenever new media metadata is available.
+    * @param metadata New metadata.
+    */
+   @Override
+        public void onPrimaryMetadataOrStateChanged(MediaMetadata mediaMetadata, int state) {
+          CharSequence title = mediaMetadata != null ? mediaMetadata.getText("android.media.metadata.TITLE") : null;
+          CharSequence artist = mediaMetadata != null ? mediaMetadata.getText("android.media.metadata.ARTIST") : null;
+
+          mMediaTitle = title;
+          mMediaArtist = artist;
+          mMediaArtwork = mediaMetadata != null ? new BitmapDrawable(mContext.getResources(), mediaMetadata.getBitmap("android.media.metadata.ALBUM_ART")) : null;
+
+          update();
+   }
+
+   public void update() {
+      updateObjects();
+      updateButtons();
+      updateViews();
+      updateIconPlayPause();
+   }
+
+   public void updateIconPlayPause() {
+       if ( !(mMediaManager.getPlaybackStateIsEqual(PlaybackState.STATE_PLAYING)) ) {
+           mPlayPause.setImageResource(R.drawable.ic_play_arrow_white);
+       } else {
+           mPlayPause.setImageResource(R.drawable.ic_pause_white);
+       }
+   }
+
+   public void updateObjects() {
+      if (mTitle == null || mArtist == null || mArtwork == null || mPrevious == null || mPlayPause == null || mNext == null) {
+          mArtwork = findViewById(R.id.artwork);
+          mTitle = (TextView) findViewById(R.id.title);
+          mArtist = (TextView) findViewById(R.id.artist);
+          mPrevious = findViewById(R.id.button_previous);
+          mPlayPause = findViewById(R.id.button_play_pause);
+          mNext = findViewById(R.id.button_next);
+      }
+   }
+
+   public void updateButtons() {
+       mPrevious.setOnClickListener(v -> {
+            mMediaManager.skipTrackPrevious();
+       });
+
+       mPlayPause.setOnClickListener(v -> {
+            mMediaManager.playPauseTrack();
+       });
+
+       mNext.setOnClickListener(v -> {
+            mMediaManager.skipTrackNext();
+       });
+   }
+
+   public void updateViews() {
+
+        boolean show = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.MUSIC_VOLUME_PANEL_DIALOG, 0, UserHandle.USER_CURRENT) != 0;
+
+        if (mMediaManager != null && mMediaTitle != null && mMediaArtist != null && mMediaArtwork != null) {
+            mTitle.setText(mMediaTitle.toString());
+            mTitle.setSelected(true);
+            mArtist.setText(mMediaArtist.toString());
+            mArtist.setSelected(true);
+
+            mArtwork.setImageDrawable(mMediaArtwork);
+
+            if (mMediaManager.getMediaMetadata().getBitmap("android.media.metadata.ALBUM_ART") != null) {
+                Palette.generateAsync((mMediaManager.getMediaMetadata().getBitmap("android.media.metadata.ALBUM_ART")), this);
+            }
+            setVisibility(show ? View.VISIBLE : View.GONE);
+        } else {
+            setVisibility(View.GONE);
+        }
+        mArtwork.setClipToOutline(true);
+   }
+
+   @Override
+   public void onGenerated(Palette palette) {
+
+        shadow = 115;
+        colorArtwork = Color.BLACK;
+        colorTextIcons = Color.WHITE;
+
+        colorTextIcons = palette.getLightVibrantColor(colorTextIcons);
+        colorArtwork = ColorUtils.setAlphaComponent(palette.getDarkVibrantColor(colorArtwork), shadow);
+
+        mArtwork.setColorFilter(colorArtwork, Mode.SRC_ATOP);
+        mTitle.setTextColor(colorTextIcons);
+        mArtist.setTextColor(colorTextIcons);
+        mPrevious.setColorFilter(colorTextIcons);
+        mPlayPause.setColorFilter(colorTextIcons);
+        mNext.setColorFilter(colorTextIcons);
+    }
+}

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -38,7 +38,8 @@ import static com.android.internal.jank.InteractionJankMonitor.CUJ_VOLUME_CONTRO
 import static com.android.internal.jank.InteractionJankMonitor.Configuration.Builder;
 import static com.android.systemui.people.PeopleSpaceUtils.convertDrawableToBitmap;
 import static com.android.systemui.volume.Events.DISMISS_REASON_SETTINGS_CLICKED;
-
+import com.android.systemui.statusbar.NotificationMediaManager;
+import com.android.systemui.spark.SparkMusic;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
@@ -180,6 +181,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             "system:" + Settings.System.VOLUME_MEDIA_OUTPUT_TOGGLE;
     public static final String CUSTOM_VOLUME_STYLES =
             "system:" + Settings.System.CUSTOM_VOLUME_STYLES;
+
+    private SparkMusic mMusicText;
+    private NotificationMediaManager mMediaManager;
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
     private static final int UPDATE_ANIMATION_DURATION = 80;
@@ -362,7 +366,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             DeviceConfigProxy deviceConfigProxy,
             Executor executor,
             DumpManager dumpManager,
-            TunerService tunerService) {
+            TunerService tunerService,
+            NotificationMediaManager mediaManager) {
         mContext =
                 new ContextThemeWrapper(context, R.style.volume_dialog_theme);
         mController = volumeDialogController;
@@ -409,6 +414,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         }
         mTunerService.addTunable(mTunable, VOLUME_MEDIA_OUTPUT_TOGGLE);
         mTunerService.addTunable(mTunable, CUSTOM_VOLUME_STYLES);
+        mMediaManager = mediaManager;
 
         initDimens();
 
@@ -606,10 +612,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mWindow.setLayout(WRAP_CONTENT, WRAP_CONTENT);
 
         mDialog.setContentView(R.layout.volume_dialog);
+        mMusicText = mDialog.findViewById(R.id.music_main);
+        mMusicText.initDependencies(mMediaManager, mContext);
         mDialogView = mDialog.findViewById(R.id.volume_dialog);
         mDialogView.setAlpha(0);
         mDialogView.setLayoutDirection(
                 mVolumePanelOnLeft ? LAYOUT_DIRECTION_LTR : LAYOUT_DIRECTION_RTL);
+        mMusicText.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
         mDialog.setCanceledOnTouchOutside(true);
         mDialog.setOnShowListener(dialog -> {
             mDialogView.getViewTreeObserver().addOnComputeInternalInsetsListener(this);
@@ -633,8 +642,19 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                         }
                     })
                     .start();
+            if (!isLandscape()) {
+            mMusicText.setTranslationX(getTranslationForPanelLocation() * mMusicText.getWidth() / 2.0f);
+            }
+            mMusicText.setAlpha(0);
+            mMusicText.animate()
+                    .alpha(1)
+                    .translationX(0)
+                    .setDuration(mDialogShowAnimationDurationMs)
+                    .setListener(getJankListener(mMusicText, TYPE_SHOW, DIALOG_TIMEOUT_MILLIS))
+                    .setInterpolator(new SystemUIInterpolators.LogDecelerateInterpolator())
+                    .start();
+            mMusicText.update();
         });
-
         mDialog.setOnDismissListener(dialogInterface ->
                 mDialogView
                         .getViewTreeObserver()
@@ -937,6 +957,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private void addRow(int stream, int iconRes, int iconMuteRes, boolean important,
             boolean defaultStream) {
         addRow(stream, iconRes, iconMuteRes, important, defaultStream, false);
+    }
+
+    public void initText (NotificationMediaManager mediaManager) {
+        mMediaManager = mediaManager;
     }
 
     private void addRow(int stream, int iconRes, int iconMuteRes, boolean important,
@@ -1870,6 +1894,12 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         }
         mDialogView.setTranslationX(0);
         mDialogView.setAlpha(1);
+        mMusicText.setTranslationX(0);
+        mMusicText.setAlpha(1);
+        ViewPropertyAnimator musicAnimator = mMusicText.animate()
+                .alpha(0)
+                .setDuration(mDialogHideAnimationDurationMs)
+                .setInterpolator(new SystemUIInterpolators.LogAccelerateInterpolator());
         ViewPropertyAnimator animator = mDialogView.animate()
                 .alpha(0)
                 .setDuration(mDialogHideAnimationDurationMs)
@@ -1886,10 +1916,16 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     hideRingerDrawer();
                     mController.notifyVisible(false);
                 }, 50));
+
         if (!shouldSlideInVolumeTray()) {
             animator.translationX(getTranslationForPanelLocation() * mDialogView.getWidth() / 2.0f);
         }
+        if (!isLandscape()) {
+            musicAnimator.translationX(getTranslationForPanelLocation() * mMusicText.getWidth() / 2.0f);
+        }
         animator.setListener(getJankListener(getDialogView(), TYPE_DISMISS,
+                mDialogHideAnimationDurationMs)).start();
+        musicAnimator.setListener(getJankListener(mMusicText, TYPE_DISMISS,
                 mDialogHideAnimationDurationMs)).start();
         checkODICaptionsTooltip(true);
         synchronized (mSafetyWarningLock) {

Ghostalioth/android

packages/SystemUI/src/com/android/systemui/nad/SparkMusic.java
@@ -1,0 +1,205 @@
+/*
+* Copyright (C) 2015 The Android Open Source Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.nad;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.Outline;
+import android.graphics.PorterDuff.Mode;
+import android.media.MediaMetadata;
+import android.media.session.MediaController;
+import android.media.session.PlaybackState;
+import android.os.UserHandle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.settingslib.Utils;
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.NotificationMediaManager;
+import lineageos.util.palette.Palette;
+import com.android.internal.graphics.ColorUtils;
+
+public class SparkMusic extends RelativeLayout implements NotificationMediaManager.MediaListener, Palette.PaletteAsyncListener {
+   private static final boolean DEBUG = true;
+   private static final String TAG = "SparkMusic";
+
+   private Context mContext;
+   private NotificationMediaManager mMediaManager;
+   private MediaController mMediaController;
+   private final Handler mHandler = new Handler();
+
+   private CharSequence mMediaTitle;
+   private CharSequence mMediaArtist;
+   private Drawable mMediaArtwork;
+   private boolean mMediaIsVisible;
+
+   private TextView mTitle;
+   private TextView mArtist;
+   private ImageView mArtwork;
+   private int shadow;
+   private int colorArtwork;
+   private int colorTextIcons;
+
+   private ImageButton mPrevious;
+   private ImageButton mPlayPause;
+   private ImageButton mNext;
+
+   private SparkMusic mBackground;
+
+   public SparkMusic(Context context) {
+       this(context, null);
+   }
+
+   public SparkMusic(Context context, AttributeSet attrs) {
+       this(context, attrs, 0);
+   }
+
+   public SparkMusic(Context context, AttributeSet attrs, int defStyleAttr) {
+       this(context, attrs, defStyleAttr, 0);
+   }
+
+   public SparkMusic(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+       super(context, attrs, defStyleAttr, defStyleRes);
+       if (DEBUG) Log.d(TAG, "New Instance");
+   }
+
+   public void initDependencies(NotificationMediaManager mediaManager, Context context) {
+      mContext = context;
+      mMediaManager = mediaManager;
+      mMediaManager.addCallback(this);
+      updateObjects();
+   }
+
+   /**
+    * Called whenever new media metadata is available.
+    * @param metadata New metadata.
+    */
+   @Override
+        public void onPrimaryMetadataOrStateChanged(MediaMetadata mediaMetadata, int state) {
+          CharSequence title = mediaMetadata != null ? mediaMetadata.getText("android.media.metadata.TITLE") : null;
+          CharSequence artist = mediaMetadata != null ? mediaMetadata.getText("android.media.metadata.ARTIST") : null;
+
+          mMediaTitle = title;
+          mMediaArtist = artist;
+          mMediaArtwork = mediaMetadata != null ? new BitmapDrawable(mContext.getResources(), mediaMetadata.getBitmap("android.media.metadata.ALBUM_ART")) : null;
+
+          update();
+   }
+
+   public void update() {
+      updateObjects();
+      updateButtons();
+      updateViews();
+      updateIconPlayPause();
+   }
+
+   public void updateIconPlayPause() {
+       if ( !(mMediaManager.getPlaybackStateIsEqual(PlaybackState.STATE_PLAYING)) ) {
+           mPlayPause.setImageResource(R.drawable.ic_play_arrow_white);
+       } else {
+           mPlayPause.setImageResource(R.drawable.ic_pause_white);
+       }
+   }
+
+   public void updateObjects() {
+      if (mTitle == null || mArtist == null || mArtwork == null || mPrevious == null || mPlayPause == null || mNext == null) {
+          mArtwork = findViewById(R.id.artwork);
+          mTitle = (TextView) findViewById(R.id.title);
+          mArtist = (TextView) findViewById(R.id.artist);
+          mPrevious = findViewById(R.id.button_previous);
+          mPlayPause = findViewById(R.id.button_play_pause);
+          mNext = findViewById(R.id.button_next);
+      }
+   }
+
+   public void updateButtons() {
+       mPrevious.setOnClickListener(v -> {
+            mMediaManager.skipTrackPrevious();
+       });
+
+       mPlayPause.setOnClickListener(v -> {
+            mMediaManager.playPauseTrack();
+       });
+
+       mNext.setOnClickListener(v -> {
+            mMediaManager.skipTrackNext();
+       });
+   }
+
+   public void updateViews() {
+
+        boolean show = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.MUSIC_VOLUME_PANEL_DIALOG, 0, UserHandle.USER_CURRENT) != 0;
+
+        if (mMediaManager != null && mMediaTitle != null && mMediaArtist != null && mMediaArtwork != null) {
+            mTitle.setText(mMediaTitle.toString());
+            mTitle.setSelected(true);
+            mArtist.setText(mMediaArtist.toString());
+            mArtist.setSelected(true);
+
+            mArtwork.setImageDrawable(mMediaArtwork);
+
+            if (mMediaManager.getMediaMetadata().getBitmap("android.media.metadata.ALBUM_ART") != null) {
+                Palette.generateAsync((mMediaManager.getMediaMetadata().getBitmap("android.media.metadata.ALBUM_ART")), this);
+            }
+            setVisibility(show ? View.VISIBLE : View.GONE);
+        } else {
+            setVisibility(View.GONE);
+        }
+        mArtwork.setClipToOutline(true);
+   }
+
+   @Override
+   public void onGenerated(Palette palette) {
+
+        shadow = 115;
+        colorArtwork = Color.BLACK;
+        colorTextIcons = Color.WHITE;
+
+        colorTextIcons = palette.getLightVibrantColor(colorTextIcons);
+        colorArtwork = ColorUtils.setAlphaComponent(palette.getDarkVibrantColor(colorArtwork), shadow);
+
+        mArtwork.setColorFilter(colorArtwork, Mode.SRC_ATOP);
+        mTitle.setTextColor(colorTextIcons);
+        mArtist.setTextColor(colorTextIcons);
+        mPrevious.setColorFilter(colorTextIcons);
+        mPlayPause.setColorFilter(colorTextIcons);
+        mNext.setColorFilter(colorTextIcons);
+    }
+}

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -37,7 +37,8 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static com.android.internal.jank.InteractionJankMonitor.CUJ_VOLUME_CONTROL;
 import static com.android.internal.jank.InteractionJankMonitor.Configuration.Builder;
 import static com.android.systemui.volume.Events.DISMISS_REASON_SETTINGS_CLICKED;
-
+import com.android.systemui.statusbar.NotificationMediaManager;
+import com.android.systemui.nad.SparkMusic;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
@@ -181,6 +182,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             "system:" + Settings.System.VOLUME_MEDIA_OUTPUT_TOGGLE;
     public static final String CUSTOM_VOLUME_STYLES =
             "system:" + Settings.System.CUSTOM_VOLUME_STYLES;
+
+    private SparkMusic mMusicText;
+    private NotificationMediaManager mMediaManager;
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
     private static final int UPDATE_ANIMATION_DURATION = 80;
@@ -368,7 +372,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             DeviceConfigProxy deviceConfigProxy,
             Executor executor,
             DumpManager dumpManager,
-            TunerService tunerService) {
+            TunerService tunerService,
+            NotificationMediaManager mediaManager) {
         mContext =
                 new ContextThemeWrapper(context, R.style.volume_dialog_theme);
         mController = volumeDialogController;
@@ -418,6 +423,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mTunerService.addTunable(mTunable, VOLUME_DIALOG_TIMEOUT);
         mTunerService.addTunable(mTunable, VOLUME_MEDIA_OUTPUT_TOGGLE);
         mTunerService.addTunable(mTunable, CUSTOM_VOLUME_STYLES);
+        mMediaManager = mediaManager;
 
         initDimens();
 
@@ -617,10 +623,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         mWindow.setLayout(WRAP_CONTENT, WRAP_CONTENT);
 
         mDialog.setContentView(R.layout.volume_dialog);
+        mMusicText = mDialog.findViewById(R.id.music_main);
+        mMusicText.initDependencies(mMediaManager, mContext);
         mDialogView = mDialog.findViewById(R.id.volume_dialog);
         mDialogView.setAlpha(0);
         mDialogView.setLayoutDirection(
                 mVolumePanelOnLeft ? LAYOUT_DIRECTION_LTR : LAYOUT_DIRECTION_RTL);
+        mMusicText.setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
         mDialog.setCanceledOnTouchOutside(true);
         mDialog.setOnShowListener(dialog -> {
             mDialogView.getViewTreeObserver().addOnComputeInternalInsetsListener(this);
@@ -644,8 +653,19 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                         }
                     })
                     .start();
+            if (!isLandscape()) {
+            mMusicText.setTranslationX(getTranslationForPanelLocation() * mMusicText.getWidth() / 2.0f);
+            }
+            mMusicText.setAlpha(0);
+            mMusicText.animate()
+                    .alpha(1)
+                    .translationX(0)
+                    .setDuration(mDialogShowAnimationDurationMs)
+                    .setListener(getJankListener(mMusicText, TYPE_SHOW, DIALOG_TIMEOUT_MILLIS))
+                    .setInterpolator(new SystemUIInterpolators.LogDecelerateInterpolator())
+                    .start();
+            mMusicText.update();
         });
-
         mDialog.setOnDismissListener(dialogInterface ->
                 mDialogView
                         .getViewTreeObserver()
@@ -958,6 +978,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private void addRow(int stream, int iconRes, int iconMuteRes, boolean important,
             boolean defaultStream) {
         addRow(stream, iconRes, iconMuteRes, important, defaultStream, false);
+    }
+
+    public void initText (NotificationMediaManager mediaManager) {
+        mMediaManager = mediaManager;
     }
 
     private void addRow(int stream, int iconRes, int iconMuteRes, boolean important,
@@ -1874,6 +1898,12 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         }
         mDialogView.setTranslationX(0);
         mDialogView.setAlpha(1);
+        mMusicText.setTranslationX(0);
+        mMusicText.setAlpha(1);
+        ViewPropertyAnimator musicAnimator = mMusicText.animate()
+                .alpha(0)
+                .setDuration(mDialogHideAnimationDurationMs)
+                .setInterpolator(new SystemUIInterpolators.LogAccelerateInterpolator());
         ViewPropertyAnimator animator = mDialogView.animate()
                 .alpha(0)
                 .setDuration(mDialogHideAnimationDurationMs)
@@ -1890,10 +1920,16 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     hideRingerDrawer();
                     mController.notifyVisible(false);
                 }, 50));
+
         if (!shouldSlideInVolumeTray()) {
             animator.translationX(getTranslationForPanelLocation() * mDialogView.getWidth() / 2.0f);
         }
+        if (!isLandscape()) {
+            musicAnimator.translationX(getTranslationForPanelLocation() * mMusicText.getWidth() / 2.0f);
+        }
         animator.setListener(getJankListener(getDialogView(), TYPE_DISMISS,
+                mDialogHideAnimationDurationMs)).start();
+        musicAnimator.setListener(getJankListener(mMusicText, TYPE_DISMISS,
                 mDialogHideAnimationDurationMs)).start();
         checkODICaptionsTooltip(true);
         synchronized (mSafetyWarningLock) {