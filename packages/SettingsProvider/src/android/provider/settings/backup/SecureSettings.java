yaap/frameworks_base

base: services: Introduce AutoDimService [1/2]
Exactly like AutoAODService but for REDUCE_BRIGHT_COLORS_ACTIVATED

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -237,5 +237,7 @@ public class SecureSettings {
         Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_TAP_GESTURE_AMBIENT,
         Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
+        Settings.Secure.EXTRA_DIM_AUTO_MODE,
+        Settings.Secure.EXTRA_DIM_AUTO_TIME,
     };
 }

Ghostalioth/android

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -240,6 +240,8 @@ public class SecureSettings {
         Settings.Secure.HEARING_AID_CALL_ROUTING,
         Settings.Secure.HEARING_AID_MEDIA_ROUTING,
         Settings.Secure.HEARING_AID_SYSTEM_SOUNDS_ROUTING,
-        Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED
+        Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
+        Settings.Secure.EXTRA_DIM_AUTO_MODE,
+        Settings.Secure.EXTRA_DIM_AUTO_TIME
     };
 }