Albinoman887/frameworks_base

QSTiles: Allow changing tile label size independently.
* Adds a secondary label size setting

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -190,6 +190,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
     private val forceHideCheveron = true
     private var labelHide = false
    private var labelSize = 15f
+   private var secondaryLabelSize = 13f
 
     init {
         setId(generateViewId())
@@ -197,6 +198,8 @@ open class QSTileViewImpl @JvmOverloads constructor(
         vertical = TileUtils.getQSTileVerticalLayout(context, if (vertical) 1 else 0)
         labelHide = TileUtils.getQSTileLabelHide(context)
         labelSize = TileUtils.getQSTileLabelSize(context)
+        secondaryLabelSize = TileUtils.getQSTileSecondaryLabelSize(context)
+
 
         importantForAccessibility = IMPORTANT_FOR_ACCESSIBILITY_YES
         clipChildren = false
@@ -234,8 +237,9 @@ open class QSTileViewImpl @JvmOverloads constructor(
     }
 
     fun updateResources() {
-        FontSizeUtils.updateFontSize(label, R.dimen.qs_tile_text_size)
-        FontSizeUtils.updateFontSize(secondaryLabel, R.dimen.qs_tile_secondary_label_text_size)
+        label.setTextSize(TypedValue.COMPLEX_UNIT_SP, labelSize)
+        secondaryLabel.setTextSize(TypedValue.COMPLEX_UNIT_SP, secondaryLabelSize)
+
 
         val iconSize = context.resources.getDimensionPixelSize(R.dimen.qs_icon_size)
         _icon.layoutParams.apply {

Ghostalioth/android

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -218,6 +218,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
     private val forceHideCheveron = true
     private var labelHide = false
    private var labelSize = 15f
+   private var secondaryLabelSize = 13f
 
     init {
         setId(generateViewId())
@@ -232,6 +233,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
 
         labelHide = QSLayoutUtils.getQSTileLabelHide(context)
         labelSize = QSLayoutUtils.getQSTileLabelSize(context)
+        secondaryLabelSize = QSLayoutUtils.getQSTileSecondaryLabelSize(context)
 
         if (labelHide)
             gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
@@ -295,8 +297,9 @@ open class QSTileViewImpl @JvmOverloads constructor(
     }
 
     fun updateResources() {
-        FontSizeUtils.updateFontSize(label, R.dimen.qs_tile_text_size)
-        FontSizeUtils.updateFontSize(secondaryLabel, R.dimen.qs_tile_secondary_label_text_size)
+        label.setTextSize(TypedValue.COMPLEX_UNIT_SP, labelSize)
+        secondaryLabel.setTextSize(TypedValue.COMPLEX_UNIT_SP, secondaryLabelSize)
+
 
 	if (isA11Style) {
 	    updateA11StyleResources()